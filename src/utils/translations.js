736a42c680f43f1fce7e3ca386372573097951fa
Victor MAZA <victor@artishoc.com>
2025-11-14 18:29:27 +0100

    update content

---
+++ src/utils/translations.js
@@ -0,0 +1,76 @@
+// Fichier de traductions pour la tombola solidaire
+// Ajoutez ici tous les textes à traduire
+
+export const translations = {
+    fr: {
+        // Titre et sous-titre
+        title: "Participez à la tombola solidaire des Amis d'Eugénie !",
+        subtitle: "Ensemble, aidons Eugénie à poursuivre ses soins médicaux.",
+        participateButton: "Je participe",
+        
+        // Section À propos d'Eugénie
+        aboutTitle: "À propos d'Eugénie",
+        aboutText1: "Eugénie Arazo est une jeune femme lumineuse qui part vivre au Mexique en 2016 pour y exercer le métier de professeur des écoles à Mexico. En 2019, elle se marie avec Damian, le couple donne naissance à leur première petite fille, Elénalice le 17 février 2022. Le 15 octobre 2024 Eugénie a été victime du HELLP syndrome, conséquence d'une complication grave d'une prééclampsie lors de sa deuxième grossesse. Son accouchement prématuré par césarienne provoque un Accident Vasculaire Cérébral entraînant une immobilité totale ainsi que diverses séquelles très importantes. Elle se bat aujourd'hui pour retrouver ses facultés mais les frais associés à son hospitalisation à domicile, non pris en charge par le système de santé mexicain, sont énormes. Elle nécessite des soins infirmiers 24h/24, des séances de kinésithérapie quotidiennes et des soins spécifiques récurrents. Aujourd'hui sans ressources, la survie de la famille et l'accompagnement médical d'Eugénie dépendent entièrement du soutien des proches.",
+        aboutText2: "L'association Les Amis d'Eugénie organise cette tombola afin d'aider à financer ses soins et soutenir sa famille.",
+        
+        // Section Le lot à gagner
+        prizeTitle: "Le lot à gagner",
+        prizeDescription: "Grâce à la générosité de l'artiste <a href=\"https://www.nortonmaza.com/\" target=\"_blank\" rel=\"noopener noreferrer\">Norton Maza</a>, vous pouvez remporter une œuvre originale.<br />Norton Maza, artiste reconnu, représentera le Chili à la prochaine Biennale de Venise en mai 2026.<br />Chaque ticket acheté rapproche Eugénie de ses soins.",
+        prizeDetails: "Titre de l'oeuvre : El Hombre en su Átomo<br />faisant partie de la série <em>Estudio para la sinfonia del poder eterno</em><br />Édition limitée - 75 exemplaires par œuvre<br />Dimensions (petit format) : 30 x 30 cm / Edition 6 de 75<br />Photographie analogique d'un diorama en impression Digigraphie Epson sur papier coton 310 g.<br />L'œuvre est numérotée, signée et accompagnée d'un certificat d'authenticité.<br />Fournie sans cadre<br />Valeur estimée 1200€",
+        
+        // Section Galerie
+        galleryTitle: "Galerie",
+        
+        // Section Comment participer
+        howToParticipateTitle: "Comment participer ?",
+        howToParticipateText1: "La participation se fait directement via HelloAsso.",
+        howToParticipateText2: "Chaque billet acheté contribue entièrement au financement des soins d'Eugénie.",
+        howToParticipateText3: "Cliquez sur le bouton ci-dessous pour accéder à la page de la tombola.",
+        participateCTA: "Participer à la tombola",
+        
+        // Section Association
+        associationTitle: "Association Les Amis d'Eugénie",
+        associationText: "L'association a pour mission de soutenir Eugénie et sa famille, moralement, matériellement et financièrement, en organisant des actions solidaires, levées de fonds et événements caritatifs.",
+        
+        // Footer
+        contact: "Contact",
+        associationLaw: "Association loi 1901",
+        copyright: "Les Amis d'Eugénie"
+    },
+    es: {
+        // Titre et sous-titre
+        title: "¡Participa en la tómbola solidaria de los Amigos de Eugénie!",
+        subtitle: "Juntos, ayudemos a Eugénie a continuar sus cuidados médicos.",
+        participateButton: "Participo",
+        
+        // Section À propos d'Eugénie
+        aboutTitle: "Sobre Eugénie",
+        aboutText1: "Eugénie Arazo es una joven mujer luminosa que se fue a vivir a México en 2016 para ejercer la profesión de maestra de escuela en Ciudad de México. En 2019, se casa con Damian, la pareja da a luz a su primera pequeña hija, Elénalice el 17 de febrero de 2022. El 15 de octubre de 2024, Eugénie fue víctima del síndrome HELLP, consecuencia de una complicación grave de preeclampsia durante su segundo embarazo. Su parto prematuro por cesárea provoca un Accidente Cerebrovascular que provoca una inmovilidad total así como diversas secuelas muy importantes. Hoy lucha por recuperar sus facultades, pero los gastos asociados a su hospitalización en casa, no cubiertos por el sistema de salud mexicano, son enormes. Necesita cuidados de enfermería 24h/24, sesiones de fisioterapia diarias y cuidados específicos recurrentes. Hoy sin recursos, la supervivencia de la familia y el acompañamiento médico de Eugénie dependen completamente del apoyo de los seres queridos.",
+        aboutText2: "La asociación Los Amigos de Eugénie organiza esta tómbola para ayudar a financiar sus cuidados y apoyar a su familia.",
+        
+        // Section Le lot à gagner
+        prizeTitle: "El premio a ganar",
+        prizeDescription: "Gracias a la generosidad del artista <a href=\"https://www.nortonmaza.com/\" target=\"_blank\" rel=\"noopener noreferrer\">Norton Maza</a>, puedes ganar una obra original.<br />Norton Maza, artista reconocido, representará a Chile en la próxima Bienal de Venecia en mayo de 2026.<br />Cada boleto comprado acerca a Eugénie a sus cuidados.",
+        prizeDetails: "Título de la obra: El Hombre en su Átomo<br />parte de la serie <em>Estudio para la sinfonia del poder eterno</em><br />Edición limitada - 75 ejemplares por obra<br />Dimensiones (formato pequeño): 30 x 30 cm / Edición 6 de 75<br />Fotografía analógica de un diorama en impresión Digigraphie Epson sobre papel algodón 310 g.<br />La obra está numerada, firmada y acompañada de un certificado de autenticidad.<br />Suministrada sin marco<br />Valor estimado 1200€",
+        
+        // Section Galerie
+        galleryTitle: "Galería",
+        
+        // Section Comment participer
+        howToParticipateTitle: "¿Cómo participar?",
+        howToParticipateText1: "La participación se realiza directamente a través de HelloAsso.",
+        howToParticipateText2: "Cada boleto comprado contribuye íntegramente a la financiación de los cuidados de Eugénie.",
+        howToParticipateText3: "Haz clic en el botón de abajo para acceder a la página de la tómbola.",
+        participateCTA: "Participar en la tómbola",
+        
+        // Section Association
+        associationTitle: "Asociación Los Amigos de Eugénie",
+        associationText: "La asociación tiene como misión apoyar a Eugénie y su familia, moral, material y financieramente, organizando acciones solidarias, recaudación de fondos y eventos benéficos.",
+        
+        // Footer
+        contact: "Contacto",
+        associationLaw: "Asociación ley 1901",
+        copyright: "Los Amigos de Eugénie"
+    }
+};
+
